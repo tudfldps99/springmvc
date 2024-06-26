10970bdb25d4251ee253962a21751630f6ba3b5b
tudfldps99 <tudfldps99@naver.com>
2024-06-27 08:54:31 +0900

    20240627 Section6 HTTP 요청 파라미터 - @RequestParam

--- src/main/java/hello/springmvc/request/RequestParamController.java
+++ src/main/java/hello/springmvc/request/RequestParamController.java
@@ -5,8 +5,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -14,9 +17,6 @@ public class RequestParamController {
 
     /**
      * 반환 타입이 없으면 이렇게 응답에 값을 직접 집어넣으면, view 조회 X
-     * @param request
-     * @param response
-     * @throws IOException
      */
     @RequestMapping("/request-param-v1")
     public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -25,5 +25,103 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+
+    /**
+     * @RequestParam 사용
+     * - 파라미터 이름으로 바인딩
+     * @ResponseBody 추가
+     * - View 조회를 무시하고, HTTP message body에 직접 해당 내용 입력
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+        @RequestParam("username") String memberName,
+        @RequestParam("age") int memberAge) {
+
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam 사용
+     * HTTP 파라미터 이름이 변수 이름과 같으면 @RequestParam(name="xx") 생략 가능
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam 사용
+     * String, int 등의 단순 타입이면 @RequestParam 도 생략 가능
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    // 파라미터 필수 여부 - requestParamRequired
+    /**
+     * @RequestParam.required
+     *  /request-param-required -> username 이 없으므로 예외
+     *
+     * 주의!
+     *  /request-param-required?username= -> 빈문자로 통과
+     *
+     * 주의!
+     *  /request-param-required
+     * int age -> null을 int에 입력하는 것은 불가능, 따라서 Integer 변경해야 함 (또는 다음에 나오는 defaultValue 사용)
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,     // @RequestParam.required: 파라미터 필수 여부. 기본값은 true
+            @RequestParam(required = false) Integer age) {      // int age가 아닌 Integer age인 이유: null을 int에 입력하는 것 불가능(500 예외), 따라서 null을 받을 수 있는 Integer로 변경하거나 또는 defaultValue 사용
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    // 기본 값 적용 - requestParamDefault
+    /**
+     * @RequestParam
+     * - defaultValue 사용
+     *
+     * 참고: defaultValue는 빈 문자의 경우에도 적용
+     * /request-param-default?username
+     * 파라미터에 값이 없는 경우 defaultValue를 사용하면 기본 값 적용 가능.
+     * 이미 기본 값이 있기 때문에 required는 의미 없음
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age) {
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    // 파라미터를 Map으로 조회하기 - requestParamMAP
+    /**
+     * @RequestParam Map, MultiValueMap
+     * Map(key=value)
+     * MultiValueMap(key=[value1, value2, ...]) ex) (key=userIds, value=[id1, id2])
+     * 
+     * 파라미터의 값이 1개가 확실하다면 Map을 사용해도 되지만, 그렇지 않다면 MultiValueMap을 사용하자
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
     }
 }
