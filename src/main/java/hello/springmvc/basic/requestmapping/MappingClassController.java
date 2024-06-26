44378a0126a415945da7ed2b6e41cebf50deeffb
tudfldps99 <tudfldps99@naver.com>
2024-06-26 09:12:04 +0900

    20240626 Section6 요청 매핑 - API 예시

---
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -0,0 +1,48 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mapping/users")       // 클래스 레벨에 매핑 정보를 두면 메서드 레벨에서 해당 정보를 조합해서 사용
+public class MappingClassController {
+
+    /**
+     * GET /mapping/users
+     */
+    @GetMapping
+    public String users() {
+        return "get users";
+    }
+
+    /**
+     * POST /mapping/users
+     */
+    @PostMapping
+    public String addUser() {
+        return "post users";
+    }
+
+    /**
+     * GET /mapping/users/{userId}
+     */
+    @GetMapping("/{userId}")
+    public String findUser(@PathVariable String userId) {
+        return "get userId=" + userId;
+    }
+
+    /**
+     * PATCH /mapping/users/{userId}
+     */
+    @PatchMapping("/{userId}")
+    public String updateUser(@PathVariable String userId) {
+        return "update userId=" + userId;
+    }
+
+    /**
+     * DELETE /mapping/users/{userId}
+     */
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable String userId) {
+        return "delete userId=" + userId;
+    }
+}
